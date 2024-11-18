8fa211f2f2abfb9d5b68d55e6bf82a43a0985531
Michael Lumanga <115460525+babaqif@users.noreply.github.com>
2024-11-18 10:31:24 +0200

    Add files via upload

---
+++ Sample Java code/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.example.Student.student;
+
+public class Main {
+    public static void main(String[] args) {
+
+        student studentt = new student("Remy",15);
+
+        for(element : studentt){
+            System.out.println(element);
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Sample Java code/src/main/java/org/example/Student/student.java
@@ -0,0 +1,11 @@
+package org.example.Student;
+
+public class student {
+    public String name;
+    public int age;
+
+    public student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
